83da9ee8c1fc0a94ed38ae274ce1303ae2130a65
Jacek Jarocki <jacek.jarocki@ista.com>
2016-11-02 14:36:35 +0100

    JJA: Added missing navigation methods.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 import {HeroService} from "../hero.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'my-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
-
-  constructor(private heroService: HeroService) { }
-
   heroes: Hero[];
   selectedHero: Hero;
 
-  getHeroes() {
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
+  getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
